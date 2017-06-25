f2179470e65037b0c0ab137200b767428a9c2961
Pietra Freitas <pietrasofia@hotmail.com>
2017-06-25 15:19:23 -0300

    Restructing architecture.

--- __init__.py
+++ __init__.py
@@ -8,3 +8,12 @@
 # item 1 (peso & lucro do item)
 # ...
 # item n (peso & lucro do item)
+
+from classes.ga import GeneticAlgorithm
+
+if __name__ == '__main__':
+
+    # Instantiating
+    heuristic = GeneticAlgorithm(None)
+
+    heuristic.calculating()
--- classes/ga.py
+++ classes/ga.py
@@ -16,3 +16,13 @@
             # Randomly select 2 chromosomes from the population
             # Perform crossover on the 2 chromosomes selected
             # Perform mutation on the chromosomes obtained
+
+
+class GeneticAlgorithm:
+
+    def __init__(self, seed):
+        self.randomSeed = seed
+
+    def calculating(self):
+        print("MAIN DO CÁLCULO")
+
