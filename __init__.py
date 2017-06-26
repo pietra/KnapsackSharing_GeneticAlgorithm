3bec8604c703d94bff4d7d6a9c26471a145f8638
Pietra Freitas <pietrasofia@hotmail.com>
2017-06-25 23:12:54 -0300

    Added instance files. File reading ready.

--- __init__.py
+++ __init__.py
@@ -9,11 +9,16 @@
 # ...
 # item n (peso & lucro do item)
 
+from sys import argv
 from classes.ga import GeneticAlgorithm
 
 if __name__ == '__main__':
 
-    # Instantiating
-    heuristic = GeneticAlgorithm(None)
+    #Instantiating
+    heuristic = GeneticAlgorithm()
 
-    heuristic.calculating()
+    if len(argv) == 2:
+        heuristic.calculating(argv[1])
+    else:
+        print("Ops, this is the right format: python __init__.py <arquivo de entrada>")
+        exit()
--- classes/ga.py
+++ classes/ga.py
@@ -5,24 +5,52 @@
 # Calculate the fitness and volume of all chromosomes
 
 # Check what percentage of the chromosomes in the population has the same fitness value
-    # More than 90% ?
-        # Yes:
-            # 90% has the same fit value and the number of generations is greater than the limit?
-                # Yes:
-                    # STOP
-                # No:
-                    # Back to: "Calculate the fitness and volume of all chromosomes"
-        # No:
-            # Randomly select 2 chromosomes from the population
-            # Perform crossover on the 2 chromosomes selected
-            # Perform mutation on the chromosomes obtained
-
+# More than 90% ?
+# Yes:
+# 90% has the same fit value and the number of generations is greater than the limit?
+# Yes:
+# STOP
+# No:
+# Back to: "Calculate the fitness and volume of all chromosomes"
+# No:
+# Randomly select 2 chromosomes from the population
+# Perform crossover on the 2 chromosomes selected
+# Perform mutation on the chromosomes obtained
 
 class GeneticAlgorithm:
+    # DATA STRUCT: List (groups) of lists (items) of tuples (weight|value)
 
-    def __init__(self, seed):
-        self.randomSeed = seed
+    def __init__(self):
+        self.numItems = None
+        self.numGroups = None
+        self.capacity = None
+        self.numItemsByGroups = None
+        self.items = None
 
-    def calculating(self):
-        print("MAIN DO CÁLCULO")
+    def readingfile(self, file):
+        f = open(file, 'r')
+        self.numItems = int(f.readline())
+        self.numGroups = int(f.readline())
+        self.capacity = int(f.readline())
+        self.numItemsByGroups = (f.readline()).split()
+        self.items = []
 
+        groupIndex = 0
+
+        for i in range(self.numGroups):
+            self.items.append([])
+
+        for numItems in self.numItemsByGroups:
+            for i in range(int(numItems)):
+                item = (f.readline()).split()
+                self.items[groupIndex].append((int(item[0]), int(item[1])))
+            groupIndex += 1
+
+        # Printing to test
+        #for i in range(self.numGroups):
+            #print(self.items[i])
+            #print("\n", len(self.items[i]))
+            #print('\n')
+
+    def calculating(self, file):
+        self.readingfile(file)
