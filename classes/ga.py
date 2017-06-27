9c457bde2d73e3251de9b0eb66b6a02e3d8d9360
Pietra Freitas <pietrasofia@hotmail.com>
2017-06-27 19:55:30 -0300

    Fixed len of population array. Parcially working. It's needed to confront against GLPK solutions.

--- classes/ga.py
+++ classes/ga.py
@@ -7,7 +7,8 @@ class GeneticAlgorithm:
     def __init__(self, seed):
         self.SIZE_POPULATION = 10
         self.SAME_FITNESS_VALUE_PERCENT = 0.9
-        self.MAX_GENERATION = 1000
+        self.MAX_GENERATION = 10
+        self.NUM_CHROMOSOMES_TOURNAMENT_SELECTION = 5
         self.seed = seed
         self.problemInstance = KnapsackSharing()
         self.population = None
@@ -16,7 +17,7 @@ class GeneticAlgorithm:
     def generateFirstPopulation(self):
 
         # POPULATION DATA STRUCT: List (chromosomes) of lists (groups) of lists (items) of numbers 0|1
-        self.population = [[] for _ in range(self.problemInstance.numGroups)]
+        self.population = [[] for _ in range(self.SIZE_POPULATION)]
 
         # Inicializing population struct
         for i in range(self.SIZE_POPULATION):
@@ -25,6 +26,7 @@ class GeneticAlgorithm:
 
         groupIndex = 0
 
+        # Populating randomly
         for chromosome in range(self.SIZE_POPULATION):  # Creating SIZE_POPULATION chromosomes
             for numItems in self.problemInstance.numItemsByGroups:  # Passing by each group
                 for j in range(int(numItems)):  # Passing by each item of group
@@ -39,6 +41,7 @@ class GeneticAlgorithm:
         volume = 0
         groupsBenefits = [0 for _ in range(self.problemInstance.numGroups)]
 
+        # Sum volumes and benefits of chromosome
         for numItems in self.problemInstance.numItemsByGroups:  # Passing by each group
             for j in range(int(numItems)):  # Passing by each item of group
                 if chromosome[groupIndex][j] == 1:  # If the item is in the bag
@@ -47,15 +50,15 @@ class GeneticAlgorithm:
             groupIndex += 1
 
         # If the volume of items in the chromosome > bag capacity
-        # Remove items from the groups with the max benefits
+        # Remove items from the groups with the max benefits until volume <= bag capacity
         if volume > self.problemInstance.capacity:
             fitInTheBag = 0
             while (fitInTheBag == 0):
                 maxBenefitGroupIndex = groupsBenefits.index(
                     max(groupsBenefits))  # Index of the group with the max benefit
-                randomItemIndex = random.randint(0,
+                randomItemIndex = random.randint(0, # Index of a random item
                                                  int(self.problemInstance.numItemsByGroups[maxBenefitGroupIndex]) - 1)
-                if (chromosome[maxBenefitGroupIndex][randomItemIndex] == 1):
+                if chromosome[maxBenefitGroupIndex][randomItemIndex] == 1: # If the item is in the bag, remove it
                     chromosome[maxBenefitGroupIndex][randomItemIndex] = 0
                     volume -= self.problemInstance.items[maxBenefitGroupIndex][randomItemIndex][0]
                     groupsBenefits[maxBenefitGroupIndex] -= \
@@ -97,11 +100,45 @@ class GeneticAlgorithm:
 
         return chromosome3, chromosome4
 
+    def tournamentSelection(self):
+
+        competitorsFitness = [0 for _ in range(self.NUM_CHROMOSOMES_TOURNAMENT_SELECTION)]
+        competitorsIndex = [0 for _ in range(self.NUM_CHROMOSOMES_TOURNAMENT_SELECTION)]
+
+        for j in range(self.NUM_CHROMOSOMES_TOURNAMENT_SELECTION):
+            randomIndex = random.randint(0, len(self.population) - 1)
+            competitorsIndex[j] = randomIndex
+            competitorsFitness[j] = self.chromosomeFitness(self.population[randomIndex])
+        winnerChromosome = self.population[competitorsIndex[competitorsFitness.index(max(competitorsFitness))]]
+
+        return winnerChromosome
+
+    def generatingNewPopulation(self):
+
+        # POPULATION DATA STRUCT: List (chromosomes) of lists (groups) of lists (items) of numbers 0|1
+        newPopulation = [[] for _ in range(self.SIZE_POPULATION)]
+
+        # Inicializing population struct
+        for i in range(self.SIZE_POPULATION):
+            for j in range(self.problemInstance.numGroups):
+                newPopulation[i].append([])
+
+        chromosomeIndex = 0
+
+        for i in range(len(self.population) // 2):
+            crossoverResult = self.crossover(self.tournamentSelection(), self.tournamentSelection())
+            newPopulation[chromosomeIndex] = crossoverResult[0]
+            newPopulation[chromosomeIndex] = crossoverResult[1]
+            chromosomeIndex += 2
+
+        self.currentGeneration += 1
+
     def calculating(self, file):
+
         self.problemInstance.readingfile(file)
         self.generateFirstPopulation()
 
-        self.crossover(self.population[0], self.population[1])
-
-        #for chromosome in range(self.SIZE_POPULATION):
-            #print(self.chromosomeFitness(self.population[chromosome]))
+        while(self.currentGeneration < self.MAX_GENERATION):
+            self.generatingNewPopulation()
+            for i in range(len(self.population)):
+                print(self.chromosomeFitness(self.population[i]))
